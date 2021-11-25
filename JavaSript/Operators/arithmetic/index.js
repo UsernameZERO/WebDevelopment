ad57ae7a7a51bde210b273c100c9309660f5a0f8
Zero <iamvamsi999@gmail.com>
2021-11-25 19:57:28 +0530

    Arithmeticoperators

---
+++ JavaSript/Operators/arithmetic/index.js
@@ -0,0 +1,19 @@
+var x=3;
+var y=10;
+
+console.log(x+y);
+console.log(x-y);
+console.log(x*y);
+console.log(x/y);
+console.log(x%y);
+console.log(3**3);
+
+console.log(x);
+console.log(++x);
+console.log(x++);
+console.log(x);
+
+console.log(y);
+console.log(--y);
+console.log(y--);
+console.log(y);
