f9226ac42612a0241b391c393af4466df1e42ae9
Zero <iamvamsi999@gmail.com>
2021-12-21 22:13:32 +0530

    started understanding views

--- Backend/nodews/contact_list/index.js
+++ Backend/nodews/contact_list/index.js
@@ -1,16 +1,25 @@
 
 const express = require('express');
+const path = require('path');
 const port = 4444;
 
-const app = express();
 
+const app = express();
+app.set('view engine','ejs');
+
+app.set('views',path.join(__dirname,'views'));
 
 app.get('/',(req,res)=>{
-    res.send('<h1>Cool! its working yeeahh...</h1>')
+    return res.render('home');
 })
-app.get('/lol',(req,res)=>{
-    res.send('<h1>Cool! its LOL working yeeahh...</h1>')
-})
+
+// app.get('/',(req,res)=>{
+//     console.log(req);
+//     res.send('<h1>Cool! its working yeeahh...</h1>')
+// })
+// app.get('/lol',(req,res)=>{
+//     res.send('<h1>Cool! its LOL working yeeahh...</h1>')
+// })
 
 app.listen(port,(err)=>{
 if (err) {
