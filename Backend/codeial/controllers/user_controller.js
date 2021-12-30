f9416b18ded45e2024822bcea27e31a467f5edba
Zero <iamvamsi999@gmail.com>
2021-12-30 20:10:42 +0530

    Manual Authentication is done

--- Backend/codeial/controllers/user_controller.js
+++ Backend/codeial/controllers/user_controller.js
@@ -3,9 +3,24 @@ const User = require("../models/users");
 module.exports.profile = (req,res)=>{
     // return res.end('<h1>Users Profile</h1>');
 
-    return res.render('profile',{
-        title : 'profile',
-    })
+    // return res.render('profile',{
+    //     title : 'profile',
+    // })
+
+    if(req.cookies.user_id){
+        User.findById(req.cookies.user_id,(err,user)=>{
+           if (user) {
+            return res.render('profile',{
+                title : 'User profile',
+                user : user,
+            });
+           }else{
+               return res.redirect('/users/signin');
+           }
+        });
+    }else{
+        return res.redirect('/users/signin');
+    }
 };
 
 module.exports.signUp=(req,res)=>{
@@ -42,5 +57,29 @@ module.exports.create_signup = (req,res)=>{
     });
 }
 module.exports.create_signin = (req,res)=>{
+    // Steps to authenticate
+    // find the user
+    User.findOne({email : req.body.email},(err,user)=>{
+        if (err) {
+            console.log('error in finding email in sign in');
+            return;
+        }
+        // handle the user
+        if(user){
+            // password mismatch
+            if(user.password != req.body.password){
+                return res.redirect('back');
+            }
+            //create cookies to it
+            res.cookie('user_id',user.id);
+            return res.redirect('/users/profile');
+        }
+        else{
+            //handle user not found
+            return res.redirect('back');
+        }
+    });
+  
+   
 
 }
