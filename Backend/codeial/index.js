124effc3221a703aedf4c7cbf414f81863f6fd98
Zero <iamvamsi999@gmail.com>
2022-01-05 22:42:05 +0530

    -

--- Backend/codeial/index.js
+++ Backend/codeial/index.js
@@ -11,7 +11,6 @@ const passport = require('passport');
 const passportLocal = require('./config/passport-local-strategy');
 const MongoStore = require('connect-mongo')(session); // Used connect-mongo@3 for installation
 
-
 app.use(express.urlencoded());
 app.use(cookieParser());
 
