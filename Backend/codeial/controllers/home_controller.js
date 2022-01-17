f630908a399f364a57397130fde27bdc204ae9a2
Zero <iamvamsi999@gmail.com>
2022-01-17 22:04:49 +0530

    displayed comments that are saved in db

--- Backend/codeial/controllers/home_controller.js
+++ Backend/codeial/controllers/home_controller.js
@@ -15,7 +15,15 @@ module.exports.home = function(req,res){
     // })
 
     // populate the user of each post
-    Post.find({}).populate('user').exec((err,posts)=>{
+    Post.find({})
+    .populate('user')
+    .populate({
+        path: 'comments',
+        populate: {
+            path: 'user',
+        }
+    })
+    .exec((err,posts)=>{
         return res.render('home',{
             title:"Home",
             posts : posts
