01b7e5f019f8eb53e2557a20da999f2bab6d048b
Zero <iamvamsi999@gmail.com>
2022-01-20 20:29:09 +0530

    Converting Async Await

--- Backend/codeial/controllers/dist/home_controller.dev.js
+++ Backend/codeial/controllers/dist/home_controller.dev.js
@@ -4,29 +4,44 @@ var Post = require("../models/posts");
 
 var User = require('../models/users');
 
-module.exports.home = function (req, res) {
-  // without using ejs we write in this way whether it is checking or not
-  //return res.end('<h1>Express is in Codeial</h1>');
-  //To use ejs
-  // Post.find({},(err,posts)=>{
-  //     return res.render('home',{
-  //         title:"Codieal | Home",
-  //         posts : posts
-  //     });
-  // })
-  // populate the user of each post
-  Post.find({}).populate('user').populate({
-    path: 'comments',
-    populate: {
-      path: 'user'
+module.exports.home = function _callee(req, res) {
+  var posts, users;
+  return regeneratorRuntime.async(function _callee$(_context) {
+    while (1) {
+      switch (_context.prev = _context.next) {
+        case 0:
+          _context.prev = 0;
+          _context.next = 3;
+          return regeneratorRuntime.awrap(Post.find({}).populate('user').populate({
+            path: 'comments',
+            populate: {
+              path: 'user'
+            }
+          }));
+
+        case 3:
+          posts = _context.sent;
+          _context.next = 6;
+          return regeneratorRuntime.awrap(User.find({}));
+
+        case 6:
+          users = _context.sent;
+          return _context.abrupt("return", res.render('home', {
+            title: "Home",
+            posts: posts,
+            all_users: users
+          }));
+
+        case 10:
+          _context.prev = 10;
+          _context.t0 = _context["catch"](0);
+          console.log('Error', _context.t0);
+          return _context.abrupt("return");
+
+        case 14:
+        case "end":
+          return _context.stop();
+      }
     }
-  }).exec(function (err, posts) {
-    User.find({}, function (err, users) {
-      return res.render('home', {
-        title: "Home",
-        posts: posts,
-        all_users: users
-      });
-    });
-  });
+  }, null, null, [[0, 10]]);
 };
--- Backend/codeial/controllers/home_controller.js
+++ Backend/codeial/controllers/home_controller.js
@@ -1,7 +1,7 @@
 const Post = require("../models/posts");
 const User = require('../models/users');
 
-module.exports.home = function(req,res){
+module.exports.home = async function(req,res){
 
     // without using ejs we write in this way whether it is checking or not
    //return res.end('<h1>Express is in Codeial</h1>');
@@ -15,25 +15,50 @@ module.exports.home = function(req,res){
     //     });
     // })
 
+
     // populate the user of each post
-    Post.find({})
-    .populate('user')
-    .populate({
-        path: 'comments',
-        populate: {
-            path: 'user',
-        }
-    })
-    .exec((err,posts)=>{
-        User.find({}, (err,users)=>{
-            return res.render('home',{
-                title:"Home",
-                posts : posts,
-                all_users: users
-            });
 
-        })
-    });
+
+    // Post.find({})
+    // .populate('user')
+    // .populate({
+    //     path: 'comments',
+    //     populate: {
+    //         path: 'user',
+    //     }
+    // })
+    // .exec((err,posts)=>{
+    //     User.find({}, (err,users)=>{
+    //         return res.render('home',{
+    //             title:"Home",
+    //             posts : posts,
+    //             all_users: users
+    //         });
+
+    //     })
+    // });
    
+    try {
+        //Populating the user of each post
+        let posts = await Post.find({})
+        .populate('user')
+        .populate({
+            path: 'comments',
+            populate: {
+                path: 'user',
+            }
+        });
 
+        let users = await User.find({});
+
+        return res.render('home',{
+        title:"Home",
+        posts: posts,
+        all_users: users
+        }); 
+    } catch (err) {
+        console.log('Error',err);
+        return;
+    }
+   
 }
